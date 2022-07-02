41ad5bb74a7eb3f36a9f0f239cc3b44a6ac8738d
Ba348 <bdubielecki@gmail.com>
2022-07-03 01:02:51 +0200

    Client service ut

---
+++ Tests/Application.UnitTests/Services/ClientService/GetActualWeatherDataAsyncTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Moq;
+using Weather.API.Common.Models.ResponseModels;
+using Xunit;
+using AutoFixture;
+
+namespace Application.UnitTests.Services.ClientService
+{
+    public class GetActualWeatherDataAsyncTests : ClientServiceBaseTests
+    {
+        [Fact]
+        public async Task Should_Return_Weather_Type_When_City_Found()
+        {
+            var responseMessage = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                ReasonPhrase = It.IsAny<string>(),
+                Content = new StringContent(
+                    "{\"location\": {\"name\": \"city\"}}"
+                )
+            };
+            GetHttpMessageHandlerMock(responseMessage);
+
+            var weatherApiClient = await _service.GetActualWeatherDataAsync(_fixture.Create<string>());
+
+            VerifyLogError(Times.Never());
+            Assert.NotNull(weatherApiClient);
+            Assert.IsType<WeatherClientResponse>(weatherApiClient);
+        }
+
+        [Fact]
+        public async Task Should_Return_Null_When_City_NotFound()
+        {
+            var responseError = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.NotFound,
+                ReasonPhrase = It.IsAny<string>(),
+                Content = new StringContent(
+                    "{\"error\":{\"message\":\"error\"}}"
+                )
+            };
+            GetHttpMessageHandlerMock(responseError);
+
+            var resultNull = await _service.GetActualWeatherDataAsync(_fixture.Create<string>());
+            VerifyLogError(Times.Once());
+            Assert.Null(resultNull);
+        }
+    }
+}
---
+++ Tests/Application.UnitTests/Services/ClientService/GetWeathersAsyncTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+
+namespace Application.UnitTests.Services.ClientService
+{
+    public class GetWeathersAsyncTests : ClientServiceBaseTests
+    {
+        [Fact]
+        public async Task Should_Return_Null_When_StatusCode_Not_Success()
+        {
+            var responseError = new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.BadRequest,
+                ReasonPhrase = It.IsAny<string>(),
+                Content = new StringContent("{\"error\":{\"message\":\"error\"}}")
+            };
+            GetHttpMessageHandlerMock(responseError);
+
+            var result = await _service.GetWeathersAsync(It.IsAny<string>(), It.IsAny<string>());
+            VerifyLogError(Times.Once());
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task Should_Return_List_of_Weathers_When_Status_Code_Success()
+        {
+            var responseError = new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                ReasonPhrase = It.IsAny<string>(),
+                Content = new StringContent("{\"location\": {\"name\": \"city\"}}")
+            };
+            GetHttpMessageHandlerMock(responseError);
+
+            var result = await _service.GetWeathersAsync(It.IsAny<string>(), It.IsAny<string>());
+            VerifyLogError(Times.Never());
+            Assert.NotNull(result);
+        }
+    }
+}
